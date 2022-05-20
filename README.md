fbc3b08c0bf721a46bc83aeea6ae60e3ca41356a
solimanabdallh <57145439+solimanabdallh@users.noreply.github.com>
2022-05-20 19:19:30 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# chat-example
+
+# for-IS-3-FCI-SCU-Students
+
+Check Deployed Demo Heroku:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://fci-chat.herokuapp.com/)
+
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use(express.static(__dirname + "/images"));
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`*** server running at http://localhost:${port}/`);
+});
